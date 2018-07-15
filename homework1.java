d9be84cf86b2c3032f590b4a1694484f31157ad5
Erica Chow <38082743+eriberry72@users.noreply.github.com>
2018-07-15 09:05:10 -0400

    Update homework1.java

--- homework1.java
+++ homework1.java
@@ -1,5 +1,6 @@
 public class homework1 {
   public static void main(String[] args) {
-    System.out.println("");
+    System.out.println("This is my first java program!");
+    background(yellow);
     }     
 }
